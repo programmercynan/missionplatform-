00a23feca4b545a342eb18828cb00d42b382a691
CynanRanjiwa <NancyWanjira2894@gmail.com>
2024-11-20 00:12:17 +0300

    stating to manage form input

---
+++ my/src/components/PrayerRequestForm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+/** 
+ * PrayerRequestForm Component
+ * collects prayer requests from users
+ */
+
+const PrayerRequestForm = () => {
+    // state to manage form input
+    const [prayerRequest, setPrayerRequest] = useState('');
+    
+}
