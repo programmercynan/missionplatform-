6a1b8fd1fe9e473bebc7881671f2fa64ed474fb0
CynanRanjiwa <NancyWanjira2894@gmail.com>
2024-11-20 01:19:51 +0300

    setting up routing in app.js

--- my/src/App.js
+++ my/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Events from './pages/Events';
+import Sermons from './pages/Sermons';
+import Donations from './pages/Donations';
+import Contact from './pages/Contact';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/events" element={<Events />} />
+        <Route path="/sermons" element={<Sermons />} />
+        <Route path="/donations" element={<Donations />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
+    
   );
-}
+};
 
 export default App;
