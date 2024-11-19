d41d9385249238dac3857a31cbc51434279ea83f
CynanRanjiwa <NancyWanjira2894@gmail.com>
2024-11-19 23:31:58 +0300

    mocking data for events

---
+++ my/src/components/EventCalendar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+/** 
+ * EventCalendar component
+ * Renders a list of events in a simple calender-like format
+ */
+
+const EventCalendar = () => {
+    // Mock data for events 
+    const events = [
+        { id: 1, date: '2024-11-20', title: 'Sunday Service' },
+        { id: 2, date: '2024-11-25', title: 'Bible Study' },
+        { id: 3, date: '2024-12-01', title: 'Youth Fellowship' },
+    ];
+}
